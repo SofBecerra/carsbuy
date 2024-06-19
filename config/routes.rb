1566f9140af01665fc81b092116435d9f11f2ca8
Cristina Loma <crislaragones@gmail.com>
2024-06-19 16:35:20 -0400

    bugs autenticacion

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users do
-    resources :cars, only: [:new, :create]
-  end
+  devise_for :users 
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
